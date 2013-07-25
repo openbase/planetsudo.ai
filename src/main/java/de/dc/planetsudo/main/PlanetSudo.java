e3a8f9bae8c123b7c81f696e048c519a5cfcb8fe
Divine Threepwood <divine@openbase.org>
2013-07-25 17:44:21 +0000

    fix some path

---
+++ src/main/java/de/dc/planetsudo/main/PlanetSudo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.dc.planetsudo.main;
+
+
+/**
+ *
+ * @author divine
+ */
+public class PlanetSudo {
+
+    /**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		Main.main(args);
+	}
+}
